ab306b6b0612de97918a7386d3aaf41d3095434e
Mark Rudolph <mark@k8ty.app>
2020-12-26 22:19:36 -0500

    more twirl templates

--- src/main/scala/app/k8ty/melvin/services/ApplicationService.scala
+++ src/main/scala/app/k8ty/melvin/services/ApplicationService.scala
@@ -10,6 +10,35 @@ object ApplicationService {
     case GET -> Root => {
       Ok(html.index())
     }
+
+    case GET -> Root / "login" => {
+      Ok("Not Implemented yet!")
+    }
+
+    case req @ POST -> Root / "login" => {
+      Ok("Not Implemented yet!")
+    }
+
+    case GET -> Root / "logout" => {
+      Ok("Not Implemented yet!")
+    }
+
+    case GET -> Root / "register" => {
+      Ok("Not Implemented yet!")
+    }
+
+    case POST -> Root / "register" => {
+      Ok("Not Implemented yet!")
+    }
+
+    case GET -> Root / "workers" => {
+      Ok("Not Implemented yet!")
+    }
+
+    case req @ POST -> "workers" /: org => {
+      Ok("Not Implemented yet!")
+    }
+
   }
 
 }
