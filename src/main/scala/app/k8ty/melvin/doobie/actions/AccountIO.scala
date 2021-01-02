058c412baa0b39f4bbb32207ceb724f951d52df4
Mark Rudolph <mark@k8ty.app>
2021-01-02 15:54:08 -0500

    Starting on AccountIO

--- src/main/scala/app/k8ty/melvin/doobie/actions/AccountIO.scala
+++ src/main/scala/app/k8ty/melvin/doobie/actions/AccountIO.scala
@@ -1,5 +1,15 @@
 package app.k8ty.melvin.doobie.actions
 
-class AccountIO {
+import app.k8ty.melvin.doobie.io.Account
+import cats.effect.IO
 
+trait AccountIO {
+  def registerAccount(id: String, password: Option[String] = None): IO[Option[Account]]
+  def getAccountById(id: String): IO[Option[Account]]
+  def addOrganizationToAccount(id: String, orgId: String): IO[Long]
+  def removeOrganizationFromAccount(id: String, orgId: String): IO[Long]
+  def removeAllOrganizationFromAccount(id: String): IO[Long]
+  def updateAccountPassword(id: String, password: String): IO[Long]
+  def deactivateAccount(id: String): IO[Long]
+  def deleteAccount(id: String): IO[Long]
 }
